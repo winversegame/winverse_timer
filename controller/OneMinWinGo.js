8aaf958cfd8b257906ffec4059a27e3b4a1d48cf
anand <vermaanand278@gmail.com>
2024-09-21 12:45:28 +0530

    dsaf

--- controller/OneMinWinGo.js
+++ controller/OneMinWinGo.js
@@ -1,15 +1,12 @@
-const { default: axios } = require("axios");
 const schedule = require("node-cron");
 
 exports.generatedTimeEveryAfterEveryOneMin = (io) => {
-  const job = schedule.schedule("* * * * * *",function () {
+  const job = schedule.schedule("* * * * * *", function () {
     const currentTime = new Date();
     const timeToSend =
       currentTime.getSeconds() > 0
         ? 60 - currentTime.getSeconds()
         : currentTime.getSeconds();
     io.emit("onemin", timeToSend);
-   
   });
 };
-
