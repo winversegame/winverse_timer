c4cf45b296bc62e413d022b0a5ac991c11344c9f
anand <vermaanand278@gmail.com>
2024-10-14 09:54:02 +0530

    dsaf

--- index.js
+++ index.js
@@ -49,7 +49,7 @@ if (x) {
     moment(new Date()).format("HH:mm:ss"),
     secondsUntilNextMinute
   );
-///////////////////
+
   setTimeout(() => {
     OneMinWinGo.generatedTimeEveryAfterEveryOneMin(io);
     generatedTimeEveryAfterEveryOneMinTRX(io);
